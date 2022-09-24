a058e65fb8def44625904f7bfc7c138d6a642939
Mostafa Samy <73782724+MattySamy@users.noreply.github.com>
2022-09-24 19:32:28 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,31 @@
-var url = require('url');
-var adr = 'http://localhost:8080';
-var q = url.parse(adr, true);
-
-var http = require('http');
-
-http.createServer(function(req, res) {
-    var word = req.url;
-    if (word.toString().includes("/")) {
-        var sp_i = word.indexOf("/");
-        res.write(word.slice(sp_i + 1));
+const express = require('express');
+const app = express();
+var uc = require('upper-case');
+let users = [];
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.post("/register", (req, res) => {
+    const { firstname, lastname } = req.body;
+    users.push(firstname, lastname);
+    res.status(200).json({
+        "message": "register success",
+        "data": users,
+    });
+});
+app.post("/login", (req, res) => {
+    const { firstname, lastname } = req.body;
+    users.push(firstname, lastname);
+    if (firstname != null || lastname != null) {
+        res.status(200).json({
+            "message": "login success ",
+            "data": users,
+        });
+    } else if (firstname == null, lastname == null) {
+        res.status(400).json({
+            "message": "login is bad request please re-enter your data",
+        });
     }
-    res.end();
-}).listen(8080);
+});
+app.listen(8080, (res, req) => {
+    console.log("app listen at https://127.0.0.1:8080")
+})
